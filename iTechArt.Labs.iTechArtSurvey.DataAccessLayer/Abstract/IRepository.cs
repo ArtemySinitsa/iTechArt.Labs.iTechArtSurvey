60b106525dcfa9d2e8455cf82efabc8a51d5809f
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2017-08-03 18:24:06 +0300

    Created DAL project with Repository interface.

---
+++ iTechArt.Labs.iTechArtSurvey.DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArt.Labs.iTechArtSurvey.DataAccessLayer.Abstract
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        ICollection<TEntity> GetAll();
+        TEntity Create(TEntity entity);
+        TEntity Find(int id);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
