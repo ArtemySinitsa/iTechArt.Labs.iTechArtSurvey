40a15e612b19e337305abce369e17bae4cd2f3a8
ArtemySinitsa <artsemi.sinitsa@itechart-group.com>
2017-08-11 16:55:37 +0300

    Updated Role manager to register via owin

--- iTechArt.Labs.iTechArtSurvey.BusinessLayer/UserManagement/SurveyRoleManager.cs
+++ iTechArt.Labs.iTechArtSurvey.BusinessLayer/UserManagement/SurveyRoleManager.cs
@@ -1,6 +1,8 @@
 ﻿using iTechArt.Labs.iTechArtSurvey.DataAccessLayer.EF;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 
 namespace iTechArt.Labs.iTechArtSurvey.BusinessLayer.UserManagement
 {
@@ -8,7 +10,13 @@ namespace iTechArt.Labs.iTechArtSurvey.BusinessLayer.UserManagement
     {
         public SurveyRoleManager(SurveyContext context) : base(new RoleStore<IdentityRole>(context))
         {
+        }
 
+        public static SurveyRoleManager Create(
+            IdentityFactoryOptions<SurveyRoleManager> options,
+            IOwinContext context)
+        {
+            return new SurveyRoleManager(context.Get<SurveyContext>());
         }
     }
 }
